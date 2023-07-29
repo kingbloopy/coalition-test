0cdf55a51295587371a0f6302c34585b0313ac44
Maggie McDonald <kingbloopy@gmail.com>
2023-07-29 17:48:55 -0600

    new images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.scss';
 import './reset.css';
-import homepic from './images/homepage.jpg';
+import mtns from './images/mtns.jpg';
+import summit from './images/summit.jpg';
+import camp from './images/camp.jpg';
 import logo from './images/logo.png';
 
 function App() {
@@ -20,19 +22,33 @@ function App() {
   return (
     <div className="App">
       <header>
-        <a href='/'><img className='logo' src={logo} /></a>
+        <a className='logo-container' href='/'>
+          <img className='logo' src={logo} />
+          <div className='logo-container__title-container'>
+            <h4>San Juan</h4>
+            <h5>Mountains</h5>
+          </div>
+        </a>
         <ul className='nav'>
-          <a className='nav__link' href='#history'>01. History</a>
-          <a className='nav__link' href='#climb'>02. Climb</a>
+          <a className='nav__link' href='#summit'>01. Summit</a>
+          <a className='nav__link' href='#camp'>02. Camp</a>
         </ul>
       </header>
 
-      <div className='section'>
-        <img className='section__background-pic' src={homepic} />
+      <div className='section' id="mtns">
+        <img className='section__background-pic' src={mtns} />
+        <div className='mtns__title-container'>
+          <h1>San Juan</h1>
+          <h2>Mountains</h2>
+        </div>
       </div>
 
-      <div className='section'>
-        <img className='section__background-pic' src={homepic} />
+      <div className='section' id='summit'>
+        <img className='section__background-pic' src={summit} />
+      </div>
+
+      <div className='section' id='camp'>
+        <img className='section__background-pic' src={camp} />
       </div>
     </div>
   );
