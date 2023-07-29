f522b355e501ce8af1b356b0ec84e5645de89e00
Maggie McDonald <kingbloopy@gmail.com>
2023-07-29 16:17:23 -0600

    start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './reset.css';
+import homepic from './images/homepage.jpg';
+import logo from './images/logo.png';
 
 function App() {
+
+  let className = "inverted";
+  let scrollTrigger = 60;
+
+  window.onscroll = () => {
+    if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+      document.getElementsByTagName("header")[0].classList.add(className);
+    } else {
+      document.getElementsByTagName("header")[0].classList.remove(className);
+    }
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <a href='/'><img className='logo' src={logo} /></a>
+        <ul className='nav'>
+          <a className='nav__link' href='#history'>01. History</a>
+          <a className='nav__link' href='#climb'>02. Climb</a>
+        </ul>
       </header>
+
+      <div className='section'>
+        <img className='section__background-pic' src={homepic} />
+      </div>
+
+      <div className='section'>
+        <img className='section__background-pic' src={homepic} />
+      </div>
     </div>
   );
 }
